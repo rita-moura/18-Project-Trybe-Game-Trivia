18ad7995269bd8bdcc9bf053fe591b03bb92080e
luc0liv <luoliveiradacunha@gmail.com>
2023-01-13 18:00:02 -0300

    Feat: criando prop para desabilitar botão

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default class Answers extends Component {
   render() {
-    const { answer, pickAnswer, index } = this.props;
+    const { answer, pickAnswer, index, isButtonDisabled } = this.props;
     let answerToRender = answer;
     if (answer.matter) {
       answerToRender = answer.answer;
@@ -13,6 +13,7 @@ export default class Answers extends Component {
         type="button"
         onClick={ () => pickAnswer() }
         data-testid={ answer.matter ? `wrong-answer-${index}` : 'correct-answer' }
+        disabled={ isButtonDisabled }
       >
         {answerToRender}
       </button>
@@ -23,4 +24,5 @@ export default class Answers extends Component {
 Answers.propTypes = {
   answer: PropTypes.string,
   pickAnswer: PropTypes.func,
+  isButtonDisabled: PropTypes.bool,
 }.isRequired;
