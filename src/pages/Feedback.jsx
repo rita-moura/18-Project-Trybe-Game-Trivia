f6ea5843456004f1a1d7dbf6f383fb5e6ad3cab5
Rita Moura <ritamoura520@gmail.com>
2023-01-15 18:47:27 -0300

    Feat: Requisito 15
    Implementa o botão de Play again, responsável por retornar a página de login, para que o usuário possa jogar novamente

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 export default class Feedback extends Component {
+  state = {
+    goLogin: false,
+  };
+
+  redirectToLogin = () => {
+    this.setState({ goLogin: true });
+  };
+
   render() {
+    const { goLogin } = this.state;
+
+    if (goLogin) return <Redirect to="/" />;
+
     return (
-      <div data-testid="feedback-text">Feedback</div>
+      <div data-testid="feedback-text">
+        <h1>Feedback</h1>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.redirectToLogin }
+        >
+          Play Again
+        </button>
+      </div>
     );
   }
 }
